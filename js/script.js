4ab1fe2b5833844e2028f99290c6a68a9d6bbffc
ics2o-2-2022 <124388837+ics2o-2-2022@users.noreply.github.com>
2023-04-24 17:44:21 +0000

    init

--- js/script.js
+++ js/script.js
@@ -3,3 +3,14 @@
 // Created by: Mr. Coxall
 // Created on: Sep 2020
 // This file contains the JS functions for index.html
+
+function myButtonClicked() {
+    // input
+  const numberGuessed = parseInt(document.getElementById('demo-input').value)
+
+  // process
+  //pass
+
+  // output
+  document.getElementById('answer').innerHTML = numberGuessed
+}
